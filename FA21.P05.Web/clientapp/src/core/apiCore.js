882d9258069f0a46ac27a5b75ddf8efaec279284
SilvinPradhan <silvin.pradhan-2@selu.edu>
2021-11-16 20:09:18 -0600

    test creating category from the client side successful

--- FA21.P05.Web/clientapp/src/core/apiCore.js
+++ FA21.P05.Web/clientapp/src/core/apiCore.js
@@ -7,3 +7,20 @@ export const getCategories = () => {
     })
     .catch((err) => console.log(err));
 };
+
+export const createCategory = (category) => {
+  return fetch(`/api/menu-categories`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(category),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
