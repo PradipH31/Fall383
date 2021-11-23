b121b875f46f51d367ae27394c4661e847abbbb9
SilvinPradhan <silvin.pradhan-2@selu.edu>
2021-11-23 17:43:37 -0600

    setting up braintree as payment gateway for clientside

---
+++ FA21.P05.Web/clientapp/src/core/BrainTreeDropIn/BrainTreeDropIn.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import dropin from "braintree-web-drop-in";
+
+export default function BraintreeDropIn(props) {
+  const { show, onPaymentCompleted } = props;
+
+  const [braintreeInstance, setBraintreeInstance] = useState(undefined);
+
+  useEffect(() => {
+    if (show) {
+      const initializeBraintree = () =>
+        dropin.create(
+          {
+            authorization: "sandbox_s9gd7m2p_vp62s592633kc5p5",
+            container: "#braintree-drop-in-div",
+          },
+          function (error, instance) {
+            if (error) console.error(error);
+            else setBraintreeInstance(instance);
+          }
+        );
+
+      if (braintreeInstance) {
+        braintreeInstance.teardown().then(() => {
+          initializeBraintree();
+        });
+      } else {
+        initializeBraintree();
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [show]);
+
+  return (
+    <div style={{ display: `${show ? "block" : "none"}` }}>
+      <div id={"braintree-drop-in-div"} />
+
+      <button
+        className={"btn btn-danger mt-2 mb-2 ml-2 mr-2"}
+        style={{ letterSpacing: "2px" }}
+        type="primary"
+        disabled={!braintreeInstance}
+        onClick={() => {
+          if (braintreeInstance) {
+            braintreeInstance.requestPaymentMethod((error, payload) => {
+              if (error) {
+                console.error(error);
+              } else {
+                const paymentMethodNonce = payload.nonce;
+                console.log("payment method nonce", payload.nonce);
+
+                alert(`Payment completed with nonce=${paymentMethodNonce}`);
+
+                onPaymentCompleted();
+              }
+            });
+          }
+        }}
+      >
+        {"Pay"}
+      </button>
+    </div>
+  );
+}
--- FA21.P05.Web/clientapp/src/core/checkout/Checkout.js
+++ FA21.P05.Web/clientapp/src/core/checkout/Checkout.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import "braintree-web";
+import BraintreeDropIn from "../BrainTreeDropIn/BrainTreeDropIn";
 
 const Checkout = ({ items }) => {
+  const [showBrainTreeDropIn, setShowBrainTreeDropIn] = useState(false);
   const getTotal = () => {
     return items.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
     }, 0);
   };
+
   return (
     <div>
       <h4>
@@ -14,21 +18,33 @@ const Checkout = ({ items }) => {
           ${getTotal()}
         </span>
       </h4>
-      {items.length > 0 ? (
-        <button
-          className="btn btn-success mt-2 mb-2"
-          style={{ letterSpacing: "2px" }}
-        >
-          Proceed to Checkout
-        </button>
-      ) : (
-        <div
-          style={{ letterSpacing: "2px" }}
-          className="alert alert-warning mt-2 mb-2"
-        >
-          Add Something to Cart...
-        </div>
-      )}
+      <div>
+        {items.length > 0 ? (
+          <button
+            className="btn btn-success mt-2 mb-2"
+            style={{ letterSpacing: "2px" }}
+            onClick={() => {
+              setShowBrainTreeDropIn(true);
+            }}
+            disabled={showBrainTreeDropIn}
+          >
+            Go to Checkout
+          </button>
+        ) : (
+          <div
+            style={{ letterSpacing: "2px" }}
+            className="alert alert-warning mt-2 mb-2"
+          >
+            Add Something to Cart...
+          </div>
+        )}
+      </div>
+      <BraintreeDropIn
+        show={showBrainTreeDropIn}
+        onPaymentCompleted={() => {
+          setShowBrainTreeDropIn(false);
+        }}
+      />
     </div>
   );
 };
